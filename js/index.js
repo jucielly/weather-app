e85a32e5658c50c57aa2bee024e1eaf04dbeb217
Jucielly Silva <juciellysouzasilva@gmail.com>
2020-08-13 13:04:51 -0300

    api data on the card done

--- js/index.js
+++ js/index.js
@@ -3,12 +3,36 @@ import { API_KEY } from './utils.js';
 const searchForm = document.querySelector('.search-form');
 
 const fetchCityData = (city) => {
-  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${API_KEY}`).then((response) => response.json())
-    .then((data) => console.log(data));
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${API_KEY}`).then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      const { name, weather, main } = data;
+      const cityList = document.querySelector('.weather-list');
+      const li = document.createElement('li');
+
+      const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${
+        weather[0].icon
+      }.svg`;
+
+      const cityCard = `
+      <div class="card">
+            <h3 class="city-name">${name}</h3>
+            <p class="temperature">${main.temp}°</p>
+            <img src='${icon}'>
+            <p class="weather">${weather[0].description}</p>
+        </div>
+      `;
+
+      li.innerHTML = cityCard;
+      cityList.appendChild(li);
+    }).catch((error) => {
+      console.log(error);
+    });
 };
 
 searchForm.addEventListener('submit', (e) => {
   e.preventDefault();
   const inputValue = searchForm['search-input'].value;
   fetchCityData(inputValue);
+  inputValue.value = '';
 });
